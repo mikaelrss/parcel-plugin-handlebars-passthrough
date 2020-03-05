8836789b1ec92dd1ff9de3391af656ce362a6776
Mikael Rino Solstad <mikaelrinosolstad@gmail.com>
2020-03-05 13:20:12 +0100

    fix: remove debug console.log

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 module.exports = bundler => {
-  console.log("Bundndndynd");
   bundler.addAssetType("hbs", require.resolve("./src/HbsAsset"));
   bundler.addAssetType("handlebars", require.resolve("./src/HbsAsset"));
 };
